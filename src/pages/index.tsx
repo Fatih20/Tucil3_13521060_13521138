2f4e921f5bc52ddf37d210a409f9dbfdefe74ee4
Fatih20 <fnri39@protonmail.com>
2023-04-10 14:25:56 +0700

    Cleanup.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useLocationMarker } from "@/customHook/useLocationMarker";
 import { Map, Marker, ZoomControl } from "pigeon-maps";
-import useWindowSize from "@rooks/use-window-size";
 import useWindowDimensions from "@/customHook/useWindowDimension";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,47 +20,45 @@ export default function Home() {
   const { width, height } = useWindowDimensions();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full relative">
-      <div className="absolute inset-0 w-full flex flex-col items-end justify-center z-10 pointer-events-none">
-        <button onClick={resetMarker} className="pointer-events-auto">
-          Reset Marker
-        </button>
-      </div>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between w-full relative">
+        <div className="absolute inset-0 w-full flex flex-col items-end justify-center z-10 pointer-events-none"></div>
 
-      <Map
-        width={width ?? 500}
-        height={height ?? 500}
-        defaultCenter={[-6.922, 107.609]}
-        defaultZoom={13}
-        onClick={({ latLng, event, pixel }) => {
-          console.log("Latlng", latLng);
-          if (isMarkerInHere(latLng)) {
-            return;
-          }
-          addLocationMarker(latLng);
-        }}
-      >
-        {locationMarkers.map((latlng) => {
-          console.log(latlng);
-          return (
-            <Marker
-              width={16}
-              height={16}
-              key={latlng.toString()}
-              anchor={latlng}
-              color={`hsl(360deg 39% 70%)`}
-            >
-              <Image
-                src={"/locationIcon.svg"}
+        <Map
+          width={width ?? 500}
+          height={height ?? 500}
+          defaultCenter={[-6.922, 107.609]}
+          defaultZoom={13}
+          onClick={({ latLng, event, pixel }) => {
+            console.log("Latlng", latLng);
+            if (isMarkerInHere(latLng)) {
+              return;
+            }
+            addLocationMarker(latLng);
+          }}
+        >
+          {locationMarkers.map((latlng) => {
+            console.log(latlng);
+            return (
+              <Marker
                 width={16}
                 height={16}
-                alt={"Location Marker"}
-              />
-            </Marker>
-          );
-        })}
-        <ZoomControl />
-      </Map>
-    </main>
+                key={latlng.toString()}
+                anchor={latlng}
+                color={`hsl(360deg 39% 70%)`}
+              >
+                <Image
+                  src={"/locationIcon.svg"}
+                  width={16}
+                  height={16}
+                  alt={"Location Marker"}
+                />
+              </Marker>
+            );
+          })}
+          <ZoomControl />
+        </Map>
+      </main>
+    </>
   );
 }
